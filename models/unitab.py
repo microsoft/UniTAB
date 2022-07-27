58ef1b0853344e7e3bf653b8c3dd7bbc9dca1598
Zhengyuan Yang <zhengyang@microsoft.com>
2022-07-27 13:39:40 -0700

    init commit

--- models/unitab.py
+++ models/unitab.py
@@ -1,8 +1,3 @@
-# Copyright (c) Aishwarya Kamath & Nicolas Carion. Licensed under the Apache License 2.0. All Rights Reserved
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
-"""
-MDETR model and criterion classes.
-"""
 from typing import Dict, Optional
 import random
 import json
